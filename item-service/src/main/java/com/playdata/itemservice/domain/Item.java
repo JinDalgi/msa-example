0853eb93fe5e461f9b4c4dd61008fb9bdc190f87
JinDalgi <dalgi5986@gmail.com>
2023-10-18 14:40:38 +0900

    create item-service

---
+++ item-service/src/main/java/com/playdata/itemservice/domain/Item.java
@@ -0,0 +1,28 @@
+package com.playdata.itemservice.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter @Builder
+@NoArgsConstructor @AllArgsConstructor
+@Table(name = "items")
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productId;
+
+    private String productName;
+
+    private Long stock;
+
+    private Long pricePerItem;
+
+
+}
